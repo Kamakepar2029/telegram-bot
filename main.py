2e071291f51adef2e9831fcc48d74e23c2753bb8
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-20 16:56:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import telebot
 import config
 import subprocess
 import os
+import requests as r
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['ls'])
+@bot.message_handler(commands=['start'])
 def welcome(message):
-  os.system('ls>ls')
-  output = subprocess.check_output(['ls'])
-  bot.send_message(message.chat.id,output)
+  keyboard = telebot.types.ReplyKeyboardMarkup(True)
+  keyboard.row('🆔 Account', '👭 Referral')
+  keyboard.row('📤 Withdraw','✅ About bot')
+  keyboard.row('🆘 Support')
+  bot.send_message(message.chat.id,config.startmessage)
+  bot.send_message(message.chat.id,'Something is wrong', reply_markup=keyboard)
 
-@bot.message_handler(commands=['kolya'])
-def kolya(message):
-  bot.send_message(message.chat.id, 'https://name.kamakepar.ru/request.pdf')
-
+  
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
@@ -27,4 +29,9 @@ def lalala(message):
   else:
     u = 'Your message was: '+str(message.text)+' :-)'
     bot.send_message(message.chat.id, u)
-bot.polling(none_stop=True)
+
+try:
+  bot.polling(none_stop=True)
+  pass
+except:
+  pass
